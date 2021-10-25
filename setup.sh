8728bfb2da7df6a713b0b8cc8c5f076ad33ae924
MhdSalih <92842416+MhdSalih@users.noreply.github.com>
2021-10-25 14:30:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
-{\colortbl;\red255\green255\blue255;\red189\green198\blue208;\red12\green14\blue18;}
-{\*\expandedcolortbl;;\cssrgb\c78824\c81961\c85098;\cssrgb\c5098\c6667\c9020;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-
-\itap1\trowd \taflags1 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil 
-\clmgf \clvertalt \clshdrawnil \clwWidth16480\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx4320
-\clmrg \clvertalt \clshdrawnil \clwWidth16480\clftsWidth3 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl200 \clpadr200 \gaph\cellx8640
-\pard\intbl\itap1\pardeftab720\partightenfactor0
-
-\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-mkdir -p ~/.streamlit/\
-echo "\\\
-[server]\\n\\\
-headless = true\\n\\\
-port = $PORT\\n\\\
-enableCORS = false\\n\\\
-\\n\\\
-" > ~/.streamlit/config.toml\cell 
-\pard\intbl\itap1\cell \lastrow\row}
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
